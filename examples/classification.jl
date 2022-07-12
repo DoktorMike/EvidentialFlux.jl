e1f7d083bed8ecdc82930737e75479f1e00512ea
Michael Green <micke.green@gmail.com>
2022-07-12 09:20:14 +0200

    docs: Made some comments in the example.

--- examples/classification.jl
+++ examples/classification.jl
@@ -1,7 +1,6 @@
 using EvidentialFlux
 using Flux
-using UnicodePlots
-
+using GLMakie
 
 function gendata(n)
     x1 = randn(Float32, 2, n)
@@ -12,17 +11,17 @@ function gendata(n)
     y3 = vcat(zeros(Float32, n), zeros(Float32, n), ones(Float32, n))
     hcat(x1, x2, x3), hcat(y1, y2, y3)'
 end
+
+# Generate data
 n = 200
 X, y = gendata(n)
 
-# See the data
-p = scatterplot(X[1, 1:n], X[2, 1:n], color = :green, width = 80, height = 30)
-scatterplot!(p, X[1, (n+1):(n+n)], X[2, (n+1):(n+n)], color = :red)
-
+# Define model
 m = Chain(Dense(2 => 30), DIR(30 => 3))
 opt = Flux.Optimise.AdamW(0.01)
 p = Flux.params(m)
 
+# Train it
 epochs = 500
 trnlosses = zeros(epochs)
 for e in 1:epochs
@@ -35,12 +34,14 @@ for e in 1:epochs
     trnlosses[e] = trnloss
     Flux.Optimise.update!(opt, p, grads)
 end
-scatterplot(1:epochs, trnlosses, width = 80, height = 30)
+GLMakie.scatter(1:epochs, trnlosses, width = 80, height = 30)
 
+# Test predictions
 α̂ = m(X)
 ŷ = α̂ ./ sum(α̂, dims = 1)
 u = uncertainty(α̂)
 
+# Show epistemic uncertainty
 GLMakie.heatmap(-5:0.1:5, -5:0.1:5, (x, y) -> uncertainty(m(vcat(x, y)))[1])
 GLMakie.scatter!(X[1, y[1, :].==1], X[2, y[1, :].==1], color = :red)
 GLMakie.scatter!(X[1, y[2, :].==1], X[2, y[2, :].==1], color = :green)
