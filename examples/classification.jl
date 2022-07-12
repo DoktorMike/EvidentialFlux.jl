daa32f4a2cfa6bb23e2bad55d9c23ea79c6f0006
Michael Green <micke.green@gmail.com>
2022-07-12 09:13:01 +0200

    docs: Updated examples for classification.

--- examples/classification.jl
+++ examples/classification.jl
@@ -4,10 +4,13 @@ using UnicodePlots
 
 
 function gendata(n)
-    x1 = Float32.(randn(2, n))
-    x2 = Float32.(randn(2, n) .+ 2)
-    y1, y2 = Float32.(ones(n)), Float32.(zeros(n))
-    hcat(x1, x2), hcat(vcat(y1, y2), 1 .- vcat(y1, y2))'
+    x1 = randn(Float32, 2, n)
+    x2 = randn(Float32, 2, n) .+ [2, 2]
+    x3 = randn(Float32, 2, n) .+ [-2, 2]
+    y1 = vcat(ones(Float32, n), zeros(Float32, 2 * n))
+    y2 = vcat(zeros(Float32, n), ones(Float32, n), zeros(Float32, n))
+    y3 = vcat(zeros(Float32, n), zeros(Float32, n), ones(Float32, n))
+    hcat(x1, x2, x3), hcat(y1, y2, y3)'
 end
 n = 200
 X, y = gendata(n)
@@ -16,7 +19,7 @@ X, y = gendata(n)
 p = scatterplot(X[1, 1:n], X[2, 1:n], color = :green, width = 80, height = 30)
 scatterplot!(p, X[1, (n+1):(n+n)], X[2, (n+1):(n+n)], color = :red)
 
-m = Chain(Dense(2 => 30), DIR(30 => 2))
+m = Chain(Dense(2 => 30), DIR(30 => 3))
 opt = Flux.Optimise.AdamW(0.01)
 p = Flux.params(m)
 
@@ -38,4 +41,8 @@ scatterplot(1:epochs, trnlosses, width = 80, height = 30)
 ŷ = α̂ ./ sum(α̂, dims = 1)
 u = uncertainty(α̂)
 
-contourplot(-5:.01:5, -5:.01:5, (x, y) -> uncertainty(m(vcat(y,x)))[1])
+GLMakie.heatmap(-5:0.1:5, -5:0.1:5, (x, y) -> uncertainty(m(vcat(x, y)))[1])
+GLMakie.scatter!(X[1, y[1, :].==1], X[2, y[1, :].==1], color = :red)
+GLMakie.scatter!(X[1, y[2, :].==1], X[2, y[2, :].==1], color = :green)
+GLMakie.scatter!(X[1, y[3, :].==1], X[2, y[3, :].==1], color = :blue)
+
