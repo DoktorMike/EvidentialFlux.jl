f13980be5d8f2787b7402d070358a5105a9559d7
Michael Green <micke.green@gmail.com>
2022-07-26 22:06:29 +0200

    bug: Fixed a bug in the versioning helperfile bump.sh.

--- bump.sh
+++ bump.sh
@@ -7,14 +7,17 @@ case "$1" in
 	"major")
 		echo Releasing a new Major version
 		perl -pi -e 's/(version = ")(\d+)\.(\d+)\.(\d+)/$1.($2+1).".".$3.".".$4/ge' Project.toml
+		git commit -am "chore: bumped version in Project.toml."
 		npx standard-version -r=major;;
 	"minor")
 		echo "Releasing a new Minor version"
 		perl -pi -e 's/(version = ")(\d+)\.(\d+)\.(\d+)/$1.$2.".".($3+1).".".$4/ge' Project.toml
+		git commit -am "chore: bumped version in Project.toml."
 		npx standard-version -r=minor;;
 	"patch")
 		echo "Releasing a new Patch version"
 		perl -pi -e 's/(version = ")(\d+)\.(\d+)\.(\d+)/$1.$2.".".$3.".".($4+1)/ge' Project.toml
+		git commit -am "chore: bumped version in Project.toml."
 		npx standard-version -r=patch;;
 	*)
 		echo "Dude, something is _off_!"
