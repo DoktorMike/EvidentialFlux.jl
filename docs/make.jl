7fc8c653de007fd875e98c0e9e69b58170ce71de
Michael Green <micke.green@gmail.com>
2024-06-17 13:57:24 +0200

    docs: redeploying docs.

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,8 @@ using Documenter
 using EvidentialFlux
 
 makedocs(sitename = "EvidentialFlux",
-         format = Documenter.HTML(),
-         modules = [EvidentialFlux])
+    format = Documenter.HTML(),
+    modules = [EvidentialFlux])
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
